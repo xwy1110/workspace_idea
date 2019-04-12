6b686ea63dc6545a606c284191934e9ab8c656ed
“xwy1110 <1540117407@qq.com>
2019-04-12 19:54:02 +0800

    第一次提交

---
+++ springboot_jpa/src/main/java/com/jk/xwy/controller/Book.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2019-2019,
+ * FileName: Book
+ * Author:   xwy
+ * Date:     2019/4/11 20:03
+ * History:
+ * zwy          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jk.xwy.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ *
+ * @author Administrator
+ * @create 2019/4/11
+ * @since 1.0.0
+ */
+@Controller
+public class Book {
+
+
+
+}
+
